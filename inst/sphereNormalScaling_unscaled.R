d3bcd305a03b6780146d56df95a10f0af8265a5d
andrewjholbrook <homefolder@Andrews-MacBook-Pro.local>
2021-02-27 12:18:20 -0800

    setup for scaling experiment

---
+++ inst/sphereNormalScaling_unscaled.R
@@ -0,0 +1,29 @@
+library(mvtnorm)
+library(coda)
+library(pracma)
+
+setwd("~/simplicialSampler/")
+
+source("R/simplicialSampler.R")
+
+#
+######
+############ experiment to find good lambda
+#####
+#
+maxIt <- 100000
+for(N in seq(from=2,to=100,by=2)) {
+  for(targetAccept in seq(from=0.05,to=0.95,length.out = 100)) {
+    output <- simplicialSampler(N=N, x0=rep(0,N), maxIt = maxIt,
+                                adaptStepSize=TRUE,
+                                adaptScales = FALSE,
+                                targetAccept = targetAccept,
+                                target = "sphericalGaussian")
+    out.mcmc <- as.mcmc(output[[1]])
+    eff <- effectiveSize(out.mcmc)
+    cat(N," ", output[[3]], " ",median(eff),
+        " ", min(eff), " ", output[[2]]," ",targetAccept,
+        " ", "sphericalGaussian",  " ", "unscaled", "\n",
+        file="inst/output/lambdaSearch.txt",
+        append=TRUE)  }
+}
